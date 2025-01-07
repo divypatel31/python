83d3db869a871403d59a527d715d9d57251a4e32
divypatel31 <divypateln@gmail.com>
2025-01-07 15:27:22 +0530

    Add files via upload

---
+++ substract of two number.py
@@ -0,0 +1,4 @@
+a=int(input("enter a 1st number:"))
+b=int(input("enter a 2nd number:"))
+
+print(a"-" b "="a-b)
---
+++ sum of two number.py
@@ -0,0 +1,5 @@
+a=int(input("enter a 1st number:"))
+b=int(input("enter a 2nd number:"))
+
+print(a,"+",b,"=",a+b)
+
